6035c1cab0eb1c517f9675f711af836e976d8052
Simanto roy <simanto.roy@gmail.com>
2026-07-30 23:43:40 +0530

    removed secret

--- weather-agent/app/config.py
+++ weather-agent/app/config.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
-    OPENAI_API_KEY:str="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"
+    OPENAI_API_KEY:str=""
     OPENAI_MODEL:str="gpt-5"
     DATABASE_URL:str=""
     WEATHER_SERVICE_URL:str="http://weather-service:8000"
